8f21746af42b37db230584cbf7cfcb6623e1db82
picsavary@mac.com <picsavary@mac.com>
2017-08-03 03:26:49 +0200

    Added content to the first name : Achaeos

--- achaeos.php
+++ achaeos.php
@@ -7,7 +7,12 @@
 <link href="web/css/style.css" rel="stylesheet"></head>
 <h1>Achæos</h1>
 <a href="index.php">Home</a>
-<div class="container">Your content will be there !</div>
+<div class="container">
+(Achaios)
+Ancêtre mythique des Achéens.	
+Fils de Xuthos et de Créüse-princesse athénienne-.
+
+Achæos était le demi-frère d’Ion par sa mère.</div>
 </body>
 </html>
 </div>
