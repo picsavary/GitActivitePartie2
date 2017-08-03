d9a2cb5d2933834c5108a6c38f530d9c5452519f
picsavary@mac.com <picsavary@mac.com>
2017-08-03 03:32:57 +0200

    Added nl2br funtion on  content string

--- achaeos.php
+++ achaeos.php
@@ -7,12 +7,13 @@
 <link href="web/css/style.css" rel="stylesheet"></head>
 <h1>Achæos</h1>
 <a href="index.php">Home</a>
-<div class="container">
-(Achaios)
+<div class="container">';
+echo nl2br('(Achaios)
 Ancêtre mythique des Achéens.	
 Fils de Xuthos et de Créüse-princesse athénienne-.
 
-Achæos était le demi-frère d’Ion par sa mère.</div>
+Achæos était le demi-frère d’Ion par sa mère.');
+echo '</div>
 </body>
 </html>
 </div>
